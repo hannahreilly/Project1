3989fd79eeea31dbf4dce534e068b163beae5a6e
Hannah Reilly <hannahreilly@Hannahs-MacBook-Pro-2.local>
2019-11-25 09:12:34 -0500

    commit 2

--- files/readme.md
+++ files/readme.md
@@ -1,14 +1,14 @@
 Restaurant Finder
 
-##Technologies : css, html, javascript, functions, animations & tbd
+## Technologies : css, html, javascript, functions, animations & tbd
 
-##Wireframe: 
+## Wireframe: 
 ![wireframe] (https://www.docdroid.net/xRnzWzT/wireframe-pdf.pdf)
 
 ##Description:
 Restaurant finder website will be used to search for restaurants in a specific city. The user will enter the website and type in the zip code of the area they want to search for. This search result will populate a list of restaurants based on that zip code. The list will allow the user to see the title, address and a photo of the restaurant. 
 
-##Functions: 
+## Functions: 
 User must be able to utilize the search box to retrieve information based on zip code or city name
 
 The search box must populate a list of restaurants based on the zip code
@@ -19,10 +19,10 @@ The nav bar must "link" to the search page and also one additional page listing 
 
 The user must be able to click on the name of the restaurant and that should bring them to the restaurants url
 
-##Stretch Goals:
+## Stretch Goals:
 Make the links actually take the user to a new page 
 
-##API Data:
+## API Data:
 Zomato api will be used to pull restaurants based on location along with images and website links
 
 {data: {…}, status: 200, statusText: "", headers: {…}, config: {…}, …}
--- files/script.js
+++ files/script.js
@@ -1,4 +1,3 @@
-
 const zomatoUrl = "https://developers.zomato.com/api";
 const api_key = '33d1c480bf5d69f93836c31489daab92';
 
@@ -7,35 +6,60 @@ window.onload = function (event) {
 
 
   const input = document.querySelector('#city')
-  const button = document.querySelector("#getRest")
+  const button = document.querySelector("button")
   const div = document.querySelector("#resInfo")
+  const searchInput = this.document.querySelector("input")
+  const section = document.querySelector('section')
 
   button.addEventListener('click', async function (event) {
     event.preventDefault()
-    let categories = input.value;
-
-
-    let API = await axios.get("https://developers.zomato.com/api/v2.1/categories", {
+    let textInput = searchInput.value;
+    const API = await axios.get("https://developers.zomato.com/api/v2.1/categories", {
       headers: { "user-key": "33d1c480bf5d69f93836c31489daab92" }
-    });
-    console.log(API)
 
-    function renderResults(response) {
-      const breakfast = response.data.categories.Breakfast[7];
-      const lunch = response.data.categories.Lunch[8];
-      const dinner = response.data.categories.Dinner[9];
+    })
 
-      const breakfastPara = document.createElement('h4');
-      const lunchPara = document.createElement('h4');
-      const dinnerPara = document.createElement('h4');
-
-      breakfastPara.innerHTML = breakfast;
-      div.appendChild(breakfastPara)
-      lunchPara.innerHTML = lunch;
-      div.appendChild(lunchPara)
-      dinnerPara.innerHTML = dinner;
-      div.appendChild(dinner)
+    locations(params){
+      return this
+        .get('locations', qs.stringify(params))
+        .then(data => data.location_suggestions);
     }
+ 
+    // let searchResponse = API.data.Search;
+    // console.log(searchResponse);
 
-      })
+    // function title(response) {
+    //   section.innerHTML = ""
+    //   for (let i = 0; i < searchResponse.length; i++) {
+    //     let searchElement = searchResponse[i].data;
+    //     console.log(searchElement);
+
+    //     let liNode = document.createElement('section')
+    //     liNode.innerHTML = `${searchElement} <div><img src = ${searchResponse[i].data}" </div>`
+    //     section.appendChild(liNode)
+    //   }
+    // }
+  }
+  )
+
+// add js for image slider 
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
+
+
 }
