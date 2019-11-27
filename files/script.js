9978977bf037051ae0fb19c368b5c031d5e510c3
Hannah Reilly <reillyhannah65@gmail.com>
2019-11-27 09:31:02 -0500

    Safety commit.

--- files/script.js
+++ files/script.js
@@ -4,7 +4,11 @@ const api_key = '33d1c480bf5d69f93836c31489daab92';
 window.onload = function (event) {
   const body = document.body;
 
-  const input = document.querySelector('#city')
+  const input = document.querySelector('input')
+  console.log(input);
+
+
+
   const button = document.querySelector("button")
   const restDiv = document.querySelector(".resInfo")
   const searchInput = document.querySelector("input")
@@ -14,80 +18,92 @@ window.onload = function (event) {
 
   button.addEventListener('click', async function (event) {
     event.preventDefault()
-    let resultsOne = await axios.get("https://developers.zomato.com/api/v2.1/location_details?entity_id=280&entity_type=city", {
-      headers: { "user-key": "33d1c480bf5d69f93836c31489daab92" },
-      method: 'GET'
-    })
+
+    const newInput = input.value;
+    console.log(newInput);
+
+    // let resultsOne = await axios.get(`https://developers.zomato.com/api/v2.1/location_details?entity_id=280&entity_type=${newInput}`, {
+    //   headers: { "user-key": "33d1c480bf5d69f93836c31489daab92" },
+    //   method: 'GET'
+    // })
 
     // let cityId = resultsOne.data.location.city_id
     // console.log(cityId);
-    console.log(resultsOne)
+    // console.log(resultsOne)
 
     let results = await axios.get("https://developers.zomato.com/api/v2.1/location_details?entity_id=280&entity_type=city", {
       headers: { "user-key": "33d1c480bf5d69f93836c31489daab92" },
       method: 'GET'
     })
 
+    console.log(results);
+    renderResults(results);
+  })
+
+  console.log(results);
+
+  // console.log(results);
+  // console.log(bestArray)
+
+  function renderResults(results) {
+    section.innerHTML = ""
+
     let forlooped = results.data.best_rated_restaurant
     let bestArray = results.data.best_rated_restaurant[0].restaurant.name
     let phoneNum = results.data.best_rated_restaurant[0].restaurant.phone_numbers
     let resImg = results.data.best_rated_restaurant[0].restaurant.featured_image
-    // console.log(results);
-    // console.log(bestArray)
 
-    function renderResults(results) {
-      section.innerHTML = ""
-      for (let i = 0; i < 9; i++) {
-        let element1 = forlooped[i].restaurant.name
-        let element2 = forlooped[i].restaurant.phone_numbers
-        let image1 = forlooped[i].restaurant.featured_image
-        let restAddress = forlooped[i].restaurant.location.address
+    for (let i = 0; i < 9; i++) {
+      let element1 = forlooped[i].restaurant.name
+      let element2 = forlooped[i].restaurant.phone_numbers
+      let image1 = forlooped[i].restaurant.featured_image
+      let restAddress = forlooped[i].restaurant.location.address
 
-        // let element3 = forlooped[i]
-        console.log(element1)
-        console.log(element2)
+      // let element3 = forlooped[i]
+      console.log(element1)
+      console.log(element2)
 
-        let restingDiv = document.createElement("div")
-        let restName = document.createElement('h3')
-        restName.innerHTML = `${element1}`
-        restName.classList.add("results")
+      let restingDiv = document.createElement("div")
+      let restName = document.createElement('h3')
+      restName.innerHTML = `${element1}`
+      restName.classList.add("results")
 
-        restingDiv.appendChild(restName)
+      restingDiv.appendChild(restName)
 
-        let phone = document.createElement('h4')
-        phone.innerHTML = `${element2}`
-        phone.classList.add("results")
-        restingDiv.appendChild(phone)
-        restingDiv.classList.add("resting")
+      let phone = document.createElement('h4')
+      phone.innerHTML = `${element2}`
+      phone.classList.add("results")
+      restingDiv.appendChild(phone)
+      restingDiv.classList.add("resting")
 
-        let address = document.createElement('h4')
-        address.innerHTML = `${restAddress}`
-        restingDiv.appendChild(address)
+      let address = document.createElement('h4')
+      address.innerHTML = `${restAddress}`
+      restingDiv.appendChild(address)
 
-        let image2 = document.createElement('img')
-        image2.src = image1
-        restingDiv.appendChild(image2)
-        image2.classList.add("imageClass")
+      let image2 = document.createElement('img')
+      image2.src = image1
+      restingDiv.appendChild(image2)
+      image2.classList.add("imageClass")
 
-        // section.appendChild(imgNode)
-        section.appendChild(restingDiv)
-      }
+      // section.appendChild(imgNode)
+      section.appendChild(restingDiv)
     }
-    renderResults()
-    // function title(response) {
-    //   section.innerHTML = ""
-    //   for (let i = 0; i < searchResult.length; i++) {
-    //     let searchElement = searchResult[i].data.location;
-    //     console.log(searchElement);
-
-    //     let liNode = document.createElement('section')
-    //     liNode.innerHTML = `${searchElement} <div><img src = ${searchResult[i].location} </div>`
-    //     section.appendChild(liNode)
-    //   }
-    // }
-    // title()
-
   }
+  renderResults()
+  // function title(response) {
+  //   section.innerHTML = ""
+  //   for (let i = 0; i < searchResult.length; i++) {
+  //     let searchElement = searchResult[i].data.location;
+  //     console.log(searchElement);
 
-  )
+  //     let liNode = document.createElement('section')
+  //     liNode.innerHTML = `${searchElement} <div><img src = ${searchResult[i].location} </div>`
+  //     section.appendChild(liNode)
+  //   }
+  // }
+  // title()
+
+
+
 }
+
